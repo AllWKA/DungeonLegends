eb1783dc22e014305d75e373c086629e538e97fb
AllWKa <bryanjaramillobaldeon@gmail.com>
2019-09-21 02:03:27 +0100

    navigation done

--- DungeonLengends/src/app/app-routing.module.ts
+++ DungeonLengends/src/app/app-routing.module.ts
@@ -3,7 +3,8 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  { path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomePageModule) },
+  { path: 'play-room', loadChildren: './play-room/play-room.module#PlayRoomPageModule' },
 ];
 
 @NgModule({
